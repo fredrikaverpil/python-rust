06bedd1b3bea93af3eaafb0154447a18aba56118
Fredrik Averpil <fredrik@averpil.com>
2021-09-24 14:07:45 +0200

    Add benchmark

--- tests.py
+++ tests.py
@@ -1,5 +1,39 @@
+import re
+import string
+import random
+
 import mylib
 
-def test_mylib():
+# Python ZIP version
+def count_doubles(val):
+    total = 0
+    # there is an improved version later on this post
+    for c1, c2 in zip(val, val[1:]):
+        if c1 == c2:
+            total += 1
+    return total
+
+
+# Python REGEXP version
+double_re = re.compile(r'(?=(.)\1)')
+
+def count_doubles_regex(val):
+    return len(double_re.findall(val))
+
+
+# Benchmark it
+# generate 1M of random letters to test it
+val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
+
+def test_pure_python(benchmark):
+    benchmark(count_doubles, val)
+
+def test_regex(benchmark):
+    benchmark(count_doubles_regex, val)
+
+def test_rust(benchmark):
+    benchmark(mylib.count_doubles, val)
+
+def test_get_result():
     data = "Hello world"
     assert mylib.get_result(data) == f"Rust says: {data}"
