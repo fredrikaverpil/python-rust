c7232e8434623607ccd5ad161d0682e5ccceb465
Fredrik Averpil <fredrik@averpil.com>
2021-09-24 11:57:10 +0200

    Move docker command into action

--- build-manylinux-wheels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e -u -x
-
-# https://github.com/pypa/manylinux
-
-docker pull quay.io/pypa/manylinux_2_24_x86_64
-docker run --rm -e PLAT=manylinux_2_24_x86_64 -v `pwd`:/io quay.io/pypa/manylinux_2_24_x86_64 /io/_manylinux.sh
